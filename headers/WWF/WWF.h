3cf3cf5edd19bda47ca169c24c8eea38350ed47e
Archer743 <61616845+Archer743@users.noreply.github.com>
2022-06-26 22:55:49 +0300

    Add files via upload

--- headers/WWF/WWF.h
+++ headers/WWF/WWF.h
@@ -11,7 +11,7 @@
 int chooseFile(std::string dictFileName, std::string cacheFileName, std::string loadFileName);
 void updateLoadFile(std::string filename);
 void updateCacheFile(struct trieNode* root);
-void addWordToDictionary(STD::string word);
+void addWordToDictionary(std::string word);
 
 void deconstructTrieToFile(struct trieNode* root, FILE *fp);
 int constructTrieFromFile(struct trieNode*&root, FILE *fp);
